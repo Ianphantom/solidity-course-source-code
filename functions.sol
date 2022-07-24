e36786e803bbbc5ed458a75ff728bdf3b2cd8f77
Ianphantom <ian25yola@gmail.com>
2022-07-25 00:22:34 +0700

    Belajar memebuat function

---
+++ functions.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract learnFunction {
+    function remoteControlOpen(bool closedDoor) public returns (bool) {}
+
+    function addValues() public view returns (uint256) {
+        uint256 a = 2;
+        uint256 b = 3;
+        uint256 result = a + b;
+        return result;
+    }
+
+    function addNewValues() public view returns (uint256) {
+        uint256 a = 1;
+        uint256 b = 2;
+        uint256 result = a + b;
+        return result;
+    }
+
+    // Latihan membuat funciton
+    function multiplyCalculator(uint256 a, uint256 b)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 result = a * b;
+        return result;
+    }
+}
