52ae06f061a518ba978f6c610dbdec45c4a0d810
Ianphantom <ian25yola@gmail.com>
2022-07-24 22:02:53 +0700

    Belajar menggunakan variabel dan type pada solidity

---
+++ variables-and-types.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract learnVariables {
+    // How to Write an integer, boolean, and string variabel
+    uint256 chocolateBar = 10;
+    uint256 storeOwner = 300;
+    bool lieDetector = true;
+    string errorMessageText = "Error! There has been a mistake";
+
+    // Soal Latihan
+    // Buat sebuah variabel wallet menggunakan integer
+    // Buat sebuah variabel spend menggunakan boolean
+    // Buah sebuah variabel nnotifySpend menggunakan string
+    // Initialize wallet dengan 500;
+    // set spend to false
+    // Notify the string value "you have spent money"
+
+    uint256 wallet = 500;
+    bool spend = true;
+    string notifySpend = "You have spent money";
+}
