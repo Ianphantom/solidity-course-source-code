b3b82daeaf04cd9c7af23e3398f15b06e0808569
Ianphantom <ian25yola@gmail.com>
2022-07-26 02:47:40 +0700

    Enums Learning

---
+++ enumsLearning.sol
@@ -0,0 +1,48 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Enum {
+    enum frenchFriesSize {
+        LARGE,
+        MEDIUM,
+        SMALL
+    }
+
+    frenchFriesSize choice;
+    frenchFriesSize constant defaultChoice = frenchFriesSize.LARGE;
+
+    function setSmall() public {
+        choice = frenchFriesSize.SMALL;
+    }
+
+    function getChoice() public view returns (frenchFriesSize) {
+        return choice;
+    }
+
+    function getDefaultChoice() public view returns (uint256) {
+        return uint256(defaultChoice);
+    }
+}
+
+contract enumExercise {
+    enum shirtColor {
+        RED,
+        WHITE,
+        GREEN,
+        BLUE
+    }
+
+    shirtColor constant defaultChoice = shirtColor.BLUE;
+    shirtColor choice;
+
+    function setWhite() public {
+        choice = shirtColor.WHITE;
+    }
+
+    function getChoice() public view returns (shirtColor) {
+        return choice;
+    }
+
+    function getDefaultChoice() public view returns (shirtColor) {
+        return defaultChoice;
+    }
+}
