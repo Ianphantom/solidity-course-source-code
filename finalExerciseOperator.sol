438f4ace2800e12e0065d81d7b90fc93ff2fb907
Ianphantom <ian25yola@gmail.com>
2022-07-25 15:06:40 +0700

    Latihan FInal Operator pada solidity

---
+++ finalExerciseOperator.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract FinalOperatorsExercise {
+    uint256 a = 300;
+    uint256 b = 12;
+    uint256 c = 47;
+
+    function finalize() public view returns (uint256) {
+        uint256 d = 23;
+        if (a >= b && b < a) {
+            d *= d;
+            return d - b;
+        }
+        return d;
+    }
+}
