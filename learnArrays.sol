2255a2fedeb8bf860ee9fa19f11db4a3ec627751
Ianphantom <ian25yola@gmail.com>
2022-07-26 02:28:48 +0700

    Belajar array pada solidity

---
+++ learnArrays.sol
@@ -0,0 +1,46 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract learnArrays {
+    uint256[] myArray;
+    uint256[] myArray2;
+    uint256[200] myFixedSizeArray;
+
+    function push(uint256 number) public {
+        myArray.push(number);
+    }
+
+    function pop() public {
+        myArray.pop();
+    }
+
+    function getLength() public view returns (uint256) {
+        return myArray.length;
+    }
+
+    function remove(uint256 i) public {
+        delete myArray[i];
+    }
+}
+
+contract ExerciseArrays {
+    uint256[] changeArray;
+
+    function push(uint256 number) public {
+        changeArray.push(number);
+    }
+
+    function removeElement(uint256 number) public {
+        uint256 temp;
+        uint256 lastIndex = getLength() - 1;
+        uint256 deleteIndex = changeArray[number];
+
+        temp = lastIndex;
+        lastIndex = deleteIndex;
+        changeArray[number] = temp;
+        changeArray.pop();
+    }
+
+    function getLength() public view returns (uint256) {
+        return changeArray.length;
+    }
+}
