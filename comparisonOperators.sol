002f3ed774f19427f5a4872ad8f5479c5da89dea
Ianphantom <ian25yola@gmail.com>
2022-07-25 14:09:17 +0700

    Comparison Operators

---
+++ comparisonOperators.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract ComparisonOperators {
+    uint256 a = 4;
+    uint256 b = 6;
+
+    function compare() public view {
+        require(a > b, "That is False");
+    }
+}
