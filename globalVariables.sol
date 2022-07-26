b85ec6a3b8994f8d298e3d003f8009bb2db7254e
Ianphantom <ian25yola@gmail.com>
2022-07-27 05:52:01 +0700

    belajar global Variables

---
+++ globalVariables.sol
@@ -0,0 +1,35 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract LedgerBalance {
+    mapping(address => uint256) balance;
+
+    function updatesBalance(uint256 newBalance) public {
+        balance[msg.sender] = newBalance;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return balance[msg.sender];
+    }
+}
+
+contract Updated {
+    function updatesBalance() public {
+        LedgerBalance ledgerBalance = new LedgerBalance();
+        ledgerBalance.updatesBalance(30);
+    }
+}
+
+contract SimpleStorage {
+    uint256 storedData;
+
+    function set(uint256 x) public {
+        // storedData = x;
+        // storedData = block.difficulty;
+        // storedData = block.timestamp;
+        storedData = block.number;
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
