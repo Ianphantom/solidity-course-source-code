37b425624b4949e6df70fb724f9b8496ee558733
Ianphantom <ian25yola@gmail.com>
2022-07-29 04:19:08 +0700

    Belajar pure and view pada solidity

---
+++ pureAndView.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract MyContract {
+    uint256 value;
+
+    function getValue() external view returns (uint256) {
+        return value;
+    }
+
+    function getNewValue() external pure returns (uint256) {
+        return 3 + 3;
+    }
+
+    function setValue(uint256 _value) external {
+        value = _value;
+    }
+}
+
+contract Exercise {
+    uint256 value;
+
+    function multiply() external pure returns (uint256) {
+        return 3 * 7;
+    }
+
+    function valuePlusThree() external view returns (uint256) {
+        return value + 3;
+    }
+}
