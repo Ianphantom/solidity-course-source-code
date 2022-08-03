0bd1da2564b59a2ff245c049e6a6ac986147bd07
Ianphantom <ian25yola@gmail.com>
2022-08-03 20:14:24 +0700

    onlyAfter Pattern

--- restricted-access.sol
+++ restricted-access.sol
@@ -15,4 +15,17 @@ contract RestrictedAccess {
     function changeOwnerAddress(address _newAddress) public onlyBy(owner) {
         owner = _newAddress;
     }
+
+    modifier onlyAfter(uint256 _time) {
+        require(block.timestamp >= _time, "Sender need to wait");
+        _;
+    }
+
+    function disownOwner()
+        public
+        onlyBy(owner)
+        onlyAfter(creationTime + 3 weeks)
+    {
+        delete owner;
+    }
 }
